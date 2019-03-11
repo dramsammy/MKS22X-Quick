f25ad273b0d2462f3d5a8163a2114278f3a6aa49
dramsammy <dramsammy@stuy.edu>
2019-03-11 11:02:34 -0400

    initial commit of partition

---
+++ partition.java
@@ -0,0 +1,8 @@
+public class partition{
+  public int partition(int data[], int start, int end){
+    int length = data.length;
+    Random x = new Random();
+    int index = x.nextInt() % length;
+    if ()
+  }
+}
