9d4c0fde40c2a02a50b1530ff97b61418c024fc4
Dramsammy <dramsammy@stuy.edu>
2019-03-11 20:47:47 -0400

    added the base for what is going to happen as this function starts will add while or for loop

--- partition.java
+++ partition.java
@@ -2,6 +2,9 @@ import java.util.*;
 public class partition{
   public int partition(int data[], int start, int end){
     int random = 1 * Math.random() * (end - start);
-
+    int middle = data[random];
+    data[random] = data[0];
+    data[0] = middle;
+    
   }
 }
