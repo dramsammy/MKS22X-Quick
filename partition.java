4cc768070b6d413d7d5eff076a783d6167ae8b8b
Dramsammy <dramsammy@stuy.edu>
2019-03-11 20:51:07 -0400

    base case

--- partition.java
+++ partition.java
@@ -1,10 +1,13 @@
 import java.util.*;
 public class partition{
   public int partition(int data[], int start, int end){
+    if (data.length == 1){
+      return 0;
+    }
     int random = 1 * Math.random() * (end - start);
     int middle = data[random];
     data[random] = data[0];
     data[0] = middle;
-    
+
   }
 }
